21c8c61b354c9e828388ca4b9b0f5bdecbaf2729
Danboru <8danang8@gmail.com>
2016-05-18 14:45:45 +0700

    Penambahan myservice

--- app/src/main/java/com/example/danboru/kelas/MainActivity.java
+++ app/src/main/java/com/example/danboru/kelas/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.danboru.kelas;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public ComponentName startService(View view) {
+
+            startService(new Intent(getBaseContext(), MyService.class));
+
+    }
+
+
+    @Override
+    public boolean stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+
 
 }
---
+++ app/src/main/java/com/example/danboru/kelas/MyService.java
@@ -0,0 +1,38 @@
+package com.example.danboru.kelas;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by Danboru on 18/05/2016.
+ * @since 14:44
+ *
+ */
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(MyService.this, "Service Mulai", Toast.LENGTH_SHORT) .show();
+
+        return START_STICKY;//apa ini ?
+    }
+
+    @Override
+    public void onDestroy() {
+
+        super.onDestroy();
+        Toast.makeText(MyService.this, "Service di Destroy", Toast.LENGTH_SHORT).show();
+
+
+    }
+}
